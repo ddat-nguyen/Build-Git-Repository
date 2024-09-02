01723dd99e48685b9756c911b4ad1a3e876e5267
ddat-nguyen <nguyenducdat3008@gmail.com>
2024-09-02 15:49:24 +0700

    new slice

--- main.go
+++ main.go
@@ -1,9 +1,24 @@
-package main 
+package main
 
-import (
-	"fmt"
-)
+import "fmt"
+
+// Slice - how to use slices in Go
+func Slice() {
+	arr := [5]int{1, 2, 3, 4, 5}
+	slice := arr[0:5] // Slice từ index 1 đến 3 (không bao gồm index 4)
+	fmt.Println("Slice from array:", slice)
+}
+
+func Make() {
+	value := make([]int, 3, 5)
+	value[0] = 1
+	value[1] = 2
+	value[2] = 3
+    fmt.Println("value form make: ", value)
+}
 
 func main() {
-	fmt.Println("main")
+	fmt.Println("Run program using slice Golang")
+	Slice() // Gọi hàm Slice để thực thi ví dụ về slice
+	Make()  // Gọi hàm Make để thực thi ví dụ về make
 }
